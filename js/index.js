82fe7c145a653721c09ceb04a98d82bdfcb3290d
Daan Banaan <daan.banaan@ehb.be>
2025-02-13 17:08:33 +0100

    bijna af

--- js/index.js
+++ js/index.js
@@ -5,19 +5,38 @@ import {
 function init() {
     console.log("Init");
     // TODO: log the document using console.log to test if you can get access to it
+    console.log('#button')
     // TODO: log the submit button. If that works, store that button in a variable
+    const button = document.querySelector('#button');
     // TODO: attach an event listener to that button and show a log "button clicked"
+    button.addEventListener("click", function () {
+
+        console.log("button clicked");
+        pullLever();
+
+    });
     // TODO: if the submit button is clicked, the lever is pulled (= call that function)
 }
 
 function pullLever() {
     // TODO: reset the machine (you may skip this step for now, and focus on getting the machine to work first)
     // TODO: spin the slot machine
+    slotMachine.spin();
     // TODO: show slots and win status
+    showSlots();
+
+
 }
 
 function showSlots() {
     // TODO: show the slot symbols in HTML
+
+
+    for (let index = 0; index < slotMachine.slots.length; index++) {
+        slots += '<span class = "' + slotMachine.slots[index] + '">' + slotMachine.slots[index] + '</span>'
+    }
+
+    document.querySelector('#result').innerHTML = slots;
 }
 
 function showGameResult() {
--- js/slotMachine.js
+++ js/slotMachine.js
@@ -4,6 +4,8 @@ export const slotMachine = {
     },
     getRandomSymbol: function () {
         // TODO: return a single random symbol
+        const index = Math.floor(Math.random() * 3);
+        return this.symbols[index];
     },
     reset: function () {
         // TODO: Empty out the slots and reset 'win'-boolean
@@ -13,6 +15,11 @@ export const slotMachine = {
     slotsAmount: 3,
     spin: function () {
         // TODO: generate three random symbols, and add them to the slots of the slotMachine
+        for (let index = 0; index < this.slotsAmount; index++) {
+            this.slots.push(this.getRandomSymbol());
+        }
+        
+        console.log(this.slots);
     },
     win: true
 };
